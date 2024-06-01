1a937be586ea657126bff677f2b1fb52641f9483
mfauzanz <bukanelu@gmail.com>
2024-06-01 10:24:21 +0700

    make two stcuts, transfertxparams and transfertx result

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -37,3 +37,19 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	}
 	return tx.Commit()
 }
+
+// transfertxparam contains the input parameter of transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// transfertxresult is the result of transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entriy"`
+	ToEntry     Entry    `json:"to_entry"`
+}
