e161f5a96de6783f26740e92c1787b302526d1e6
mfauzanz <bukanelu@gmail.com>
2024-06-01 10:05:39 +0700

    make storage.go, and make newstore

---
+++ db/sqlc/store.go
@@ -0,0 +1,15 @@
+package simplebank
+
+import "database/sql"
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
