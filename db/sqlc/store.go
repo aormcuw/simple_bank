4177060bb1c68ded3928b2a52c97eadf54999e3d
mfauzanz <bukanelu@gmail.com>
2024-06-01 10:16:50 +0700

    make func exectx

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,15 +1,39 @@
 package simplebank
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
 
+// Store provides all function to execute db queries and transactions
 type Store struct {
 	*Queries
 	db *sql.DB
 }
 
+// create a new store
 func NewStore(db *sql.DB) *Store {
 	return &Store{
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+// exectx executes a function wihtin a database transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err : %v, rb err : %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
 }
