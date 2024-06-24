32b140861649eba48560330b2f13cf882725c3ec
mfauzanz <bukanelu@gmail.com>
2024-06-24 15:20:34 +0700

    banyak perubahan sih, dari pembuatan account.go dll

--- api/server.go
+++ api/server.go
@@ -17,7 +17,17 @@ func NewServer(store *simplebank.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	//add routes to router
+	router.POST("/accounts", server.createAccount)
+
 	server.router = router
 	return server
+}
+
+// start run the http server on specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
