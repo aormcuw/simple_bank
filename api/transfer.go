abfe6525785ab23acd2a03638b2efbb843deb619
mfauzanz <bukanelu@gmail.com>
2024-07-18 14:04:31 +0700

    make func validAccount in transfer.go

--- api/transfer.go
+++ api/transfer.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 	db "simplebank/db/sqlc"
 
@@ -21,6 +23,13 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 
+	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+	if !server.validAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+
 	arg := db.TransferTxParams{
 		FromAccountID: req.FromAccountID,
 		ToAccountID:   req.ToAccountID,
@@ -33,4 +42,22 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, result)
+}
+
+func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currecny mismatch : %s vs %s ", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+	return true
 }
